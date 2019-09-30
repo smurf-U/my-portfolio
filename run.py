13cd70f0fab1ad63fd446f1282157de893938299
Kaushal Prajapati <kbprajapati@live.com>
2019-09-30 22:53:01 +0530

    [IMP]compress image file

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 from app import app
-from socket import gethostname
 
-if __name__ == '__main__':
-    db.create_all()
-    if 'liveconsole' not in gethostname():
-        app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8080)
